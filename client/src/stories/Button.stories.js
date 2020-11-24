06fc5ee93312a7307393166764fc4c9ba64f768f
SvLowman <SvenLohmann@gmx.de>
2020-11-24 12:24:40 +0100

    Add ESLint Rule to warn unused vars

--- client/src/stories/Button.stories.js
+++ client/src/stories/Button.stories.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { Button } from "./Button";
 
-export default {
+const hallo = {
   title: "Example/Button",
   component: Button,
   argTypes: {
@@ -34,3 +34,5 @@ Small.args = {
   size: "small",
   label: "Button",
 };
+
+export default hallo;
